cc490bf2a4a8bdb5400a1f43cf8f2d530cde028e
philippexe <philipp@fellenberg.net>
2017-01-04 18:26:07 +0100

    div id badges hinzugefügt
    nicht fertig

--- src/assets/main.js
+++ src/assets/main.js
@@ -4,8 +4,22 @@ $(function() {
       url: 'https://www.codeschool.com/users/philippexe.json',
       dataType: 'jsonp',
       success: function(response) {
-
-
+        console.log('response', response.courses.completed);
+        addCourses(response.courses.completed);
       }
     });
+
+    function addCouses(courses) {
+    var $abzeichen = $('#badges');
+    courses.forEach(function(course) {
+      $('<div />', {
+        'class': 'course'
+      }).appendTo($abzeichen);
+
+      $('<h3 />', {
+        text: course.title
+      }).appendTo($course);
+    })
+
+  }
 });
